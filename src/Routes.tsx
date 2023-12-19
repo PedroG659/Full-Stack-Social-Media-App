0add94e648c6e55f0a8f81370bc6d9ac7837fbb4
PedroG659 <150958889+PedroG659@users.noreply.github.com>
2023-12-19 11:10:44 -0300

    Update

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,17 +1,17 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 
-import SigninForm from "./_auth/forms/SigninForm";
-import SignupForm from "./_auth/forms/SignupForm";
-import AuthLayout from "./_auth/AuthLayout";
-import RootLayout from "./_root/RootLayout";
-import { Home } from './_root/pages/Home'
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import { Home } from './_root/pages'
 import './global.css';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
 
 const App = () => {
   return (
     <main className="flex h-screen">
       <Routes>
-
+        {/* public routes */}
         <Route element={<AuthLayout />}>
           <Route path="/sign-in" element={<SigninForm />} />
           <Route path="/sign-up" element={<SignupForm />} />
@@ -19,8 +19,8 @@ const App = () => {
 
 
         <Route element={<RootLayout />}>
-          <Route index element={<Home />} />
-          </Route>
+        <Route index element={<Home />} />
+        </Route>
       </Routes>
     </main>
   )
